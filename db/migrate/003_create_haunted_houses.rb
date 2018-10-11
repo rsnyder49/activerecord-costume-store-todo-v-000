d02b870c1b3043658ed30642cc926c83fa22a407
Robert Snyder <r.snyder49@gmail.com>
2018-10-11 18:48:18 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,12 @@
-# Create your haunted_houses migration here
+ # Create your haunted_houses migration here
+
+class CreateHauntedHouses < ActiveRecord::Migration 
+  def change 
+    create_table :haunted_houses do |t| 
+      t. :name 
+      t. :location 
+      t. :theme 
+      t. :price 
+      t. 
+  end 
+end 
